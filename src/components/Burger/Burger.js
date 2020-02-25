56d70e40d498f2c5e98a4d0f3b374fedc9cf2d8e
Mr. Darkside <rachel@Mrs-MacBook-Pro.local>
2020-02-25 16:23:21 -0600

    Added explanation of code to Burger.js

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -4,8 +4,18 @@ import classes from './Burger.css';
 
 const burger = (props) => {
     const transformedIngredients = Object.keys(props.ingredients).map(igKey => {
-        return [...Array(props.ingredients[igKey])].map((_, i) => {
+        //Extracting keys of a give obj & turning that into an array. --->Array of keys
+        //Map then executes a function on each element in the array.
+            //Transform the string value from igKey into an array with as many elements as there are ingredients for a given ingredient.
+                //Ex: If I have 2 cheese ingredients, I want to transform that cheese string into an array with 2 elements. The element doesn't matter, just the length of the array.
+        return [...Array(props.ingredients[igKey]
+            //Make a new array with length of props.ingredients[igKey]. (Amount of the given ingredient)
+            )].map((_, i) => {
+                //_ is an arg name to indicate a blank, since the element itself doesn't matter. i is the index of the element.
             return <BurgerIngredient key={igKey + i} type={igKey} />;
+            //Key--makes the unique key
+                //Ex: key={igKey + i} equals key={cheese +1} equals key={cheese1}
+            
         });
     });
 
